8e15ab3d2b558535f6ab934fd8bb81fba153fd15
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2024-11-08 15:59:57 +0530

    settingup_env_variables_corsPolicy

--- server.js
+++ server.js
@@ -5,22 +5,40 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const morgan = require("morgan");
-const fs=require('fs');
+const fs = require('fs');
+
 const connectDB = require("./mongoDb/db");
+
 const environment = process.env.NODE_ENV || 'development';
 console.log(`Node.js environment: ${environment}`);
 
-
 connectDB();
 
-//middlewares
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
+console.log('allowedOrigins:', allowedOrigins);
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);  // Allow the origin
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+  credentials: true
+};
+
+// Middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(cors());
+app.use(cors(corsOptions));
 
-//routes
+// Routes
 const directoryPath = './routes';
 fs.readdir(directoryPath, (err, files) => {
   if (err) {
@@ -29,25 +47,20 @@ fs.readdir(directoryPath, (err, files) => {
   }
 
   files.forEach((file) => {
-    const filePath =directoryPath+'/'+ file;
-    const routes=require(filePath);
-    
-    //routes middlewares
+    const filePath = directoryPath + '/' + file;
+    const routes = require(filePath);
+
     app.use('/api', routes);
-    console.log('imported routes',filePath);
+    console.log('imported routes', filePath);
   });
 });
 
 app.get('/api/test', (req, res) => {
-
-  // Logic for handling the GET request goes here
-  res.send('GET request received'); // Example response
+  res.send('GET request received');
 });
 
-//port
+// Port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Submit Jokes Microservice running on ${port}`);
+  console.log(`Moderate Jokes Microservice running on ${port}`);
 });
-
-
--- services/joke.js
+++ services/joke.js
@@ -1,11 +1,14 @@
+
 const { default: axios } = require("axios");
+const DELIVER_JOKES_API_URL = process.env.DELIVER_JOKES_API_URL;
 
 exports.getJokeTypes_srv = async () => {
   try {
-    const response = await axios.get("http://localhost:3333/jokes/types");
+    const response = await axios.get(`${DELIVER_JOKES_API_URL}/jokes/types`);
 
     return response.data;
   } catch (err) {
+    console.error("Error fetching joke types:", err);
     throw err;
   }
 };
